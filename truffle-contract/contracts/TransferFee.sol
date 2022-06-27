69ed69c18450c0171b89fa1df1f87507e732d271
Boyeon <dlqhdus628@naver.com>
2022-06-27 20:43:52 +0900

    contract 구조 및 이벤트 수정

--- truffle-contract/contracts/TransferFee.sol
+++ truffle-contract/contracts/TransferFee.sol
@@ -2,37 +2,54 @@
 pragma solidity ^0.8.0;
 
 contract TransferFee{
-    event TransferToContract(address from, address to, uint fee);
-    event TransferToOwner(address from, address to, uint fee);
+    event TransferToContract(address buyer, address indexed owner, uint indexed product_id, uint indexed date, uint reservationId);
+    event TransferToOwner(address buyer, address indexed owner, uint indexed product_id);
 
-    mapping (address => mapping (address => uint)) public buyerBalances;
+    struct ReservationInfo {
+        address owner;
+        address buyer;
+        uint product_id;
+        uint date;
+        uint price;
+    }
+
+    mapping (uint => ReservationInfo) public ReservationMapping;
+
+    uint private id = 0;
 
     receive() external payable {
     }
 
     /// @dev : 예약자가 contract로 송금하는 함수
-    function transferToContract(address _owner) public payable {
+    function transferToContract(address owner, uint product_id, uint date) public payable {
         require(msg.sender.balance >= msg.value, "There is not enough klay in the account");
         require(msg.value > 0, "You cannot send 0 klay");
 
-        payable(address(this)).transfer(msg.value);
-        buyerBalances[msg.sender][_owner] += msg.value;
+        uint reservationId = id;
 
-        emit TransferToContract(msg.sender, address(this), msg.value);
+        payable(address(this)).transfer(msg.value);
+        ReservationMapping[id] = ReservationInfo(owner, msg.sender, product_id, date, msg.value);
+        id += 1;
+
+        emit TransferToContract(msg.sender, owner, product_id, date, reservationId);
     }
 
     /// @dev : contract에서 집주인에게 송금하는 함수
-    function transferToOwner(address payable _owner, address _buyer) public payable {
-        require(_owner > address(0), "This is not a valid address");
-        require(buyerBalances[_buyer][_owner] <= address(this).balance, "There is no money to transfer to the contract.");
+    function transferToOwner(uint reservationId) public payable {
+        require(ReservationMapping[reservationId].price <= address(this).balance, "There is no money to transfer to the contract.");
 
-        _owner.transfer(buyerBalances[_buyer][_owner]);
+        uint fee = ReservationMapping[reservationId].price;
+        address owner = ReservationMapping[reservationId].owner;
+        address buyer = ReservationMapping[reservationId].buyer;
+        uint product_id = ReservationMapping[reservationId].product_id;
 
-        emit TransferToOwner(address(this), _owner, buyerBalances[_buyer][_owner]);
+        payable(owner).transfer(fee);
+        delete ReservationMapping[reservationId];
+
+        emit TransferToOwner(buyer, owner, product_id);
     }
 
     function getBalanceOfContract() public view returns (uint) {
         return address(this).balance;
     }
-
 }
