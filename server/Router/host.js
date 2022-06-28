fc359c2d397580e6886407c339f9aafc20862e44
Boyeon <dlqhdus628@naver.com>
2022-06-28 14:16:06 +0900

    /host/reservation-status api 구현

--- server/Router/host.js
+++ server/Router/host.js
@@ -84,4 +84,73 @@ router.post("/register", upload.single("file"), async (req, res, next) => {
   }
 });
 
+const getDate = (date) => {
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+
+  const dateString = year + "/" + month + "/" + day;
+  return dateString;
+};
+
+router.get("/reservation-status", async (req, res) => {
+  try {
+    const account = req.query.account;
+
+    const tempArray = [];
+    const reserveStatusArray = [];
+
+    const res_sql = `SELECT * FROM Reservation WHERE owner_account = '${account}'`;
+    con.query(res_sql, async (res_err, res_rows, fields) => {
+      if (res_err) {
+        res.send({ success: fail, message: res_err });
+        return;
+      } else {
+        for (let i = 0; i < res_rows.length; i++) {
+          const product_id = res_rows[i].product_id;
+          const checkin = getDate(new Date(res_rows[i].checkin));
+          const checkout = getDate(new Date(res_rows[i].checkout));
+          const reservation_day = res_rows[i].reservation_day;
+
+          const temp = {
+            product_id: product_id,
+            checkin: checkin,
+            checkout: checkout,
+            reservation_day: reservation_day,
+          };
+          tempArray.push(temp);
+        }
+
+        for (let j = 0; j < tempArray.length; j++) {
+          const product_id = tempArray[j].product_id;
+          const pro_sql = `SELECT * FROM Products WHERE id = ${product_id}`;
+          con.query(pro_sql, async (err, rows, fields) => {
+            const data = fs.readFileSync(rows[0].product_image);
+            const b64 = data.toString("base64");
+            const imgFile = `data:image/jpeg;base64,${b64}`;
+
+            const price = rows[0].price;
+
+            const result = {
+              id: product_id,
+              image: imgFile,
+              name: rows[0].product_name,
+              checkin: tempArray[j].checkin,
+              checkout: tempArray[j].checkout,
+              totalPrice: tempArray[j].reservation_day * price,
+            };
+            reserveStatusArray.push(result);
+            if (j === tempArray.length - 1) {
+              res.send({ success: true, reserveStatusArray: reserveStatusArray });
+            }
+          });
+        }
+      }
+    });
+  } catch (err) {
+    res.send({ success: fail, message: err });
+    return;
+  }
+});
+
 module.exports = router;
