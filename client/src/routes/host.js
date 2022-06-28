77f76843a680e183907630f352e1deb1ec180993
Boyeon <dlqhdus628@naver.com>
2022-06-28 14:08:24 +0900

    url 수정

--- client/src/routes/host.js
+++ client/src/routes/host.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Host = () => {
   return (
     <Flex alignItems="center" w="70vw" h="75vh" justifyContent="space-evenly">
-      <Link to="/host/reservation_status">
+      <Link to="/host/reservation-status">
         <Box w="25vw" h="45vh" bg="pink.50" borderRadius={30} textAlign="center" lineHeight="45vh">
           <Text fontWeight="bold" fontSize="5xl" color="#495057">
             예약 현황
